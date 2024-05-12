3467bede29344469459355a12c49f10ed1ec8be1
Udari Dayarathne <it20056452@my.sliit.lk>
2024-05-12 18:37:16 +0530

    search

--- admin/src/views/Inventory-view/Inventory-details/InventoryDetails.jsx
+++ admin/src/views/Inventory-view/Inventory-details/InventoryDetails.jsx
@@ -5,14 +5,25 @@ import { inventoryColumns } from "../../../data/dataTable";
 import { getAllInventories } from "../../../api/inventory/inventoryServices";
 
 const InventoryDetails = () => {
-  const [inventory, setInventory] = useState([])
+  const [inventory, setInventory] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     getAllInventories().then((res) => {
-      setInventory(res.data)
-    })
-    
-  },[])
+      setInventory(res.data);
+    });
+  }, []);
+
+  // Function to handle search query changes
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  // Filter inventory based on search query
+  const filteredInventory = inventory.filter((item) =>
+    item.inventoryName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="flex">
       <div className="flex-[7] w-full">
@@ -23,9 +34,19 @@ const InventoryDetails = () => {
             </button>
           </Link>
         </div>
+        
+        {/* Search input field */}
+        <input
+          type="text"
+          placeholder="Search inventory..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="mt-5 p-2 border rounded-md"
+        />
 
+        {/* Display filtered inventory */}
         <List
-          response={inventory}
+          response={filteredInventory}
           title={"Inventory"}
           dataCols={inventoryColumns}
         />
